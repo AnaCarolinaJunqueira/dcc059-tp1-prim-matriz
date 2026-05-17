66fdfc7e59811551c130fe9ea053f1b4d3b61b5b
Ana Carolina Junqueira e Silva <anacarolina.junqueira@estudante.ufjf.br>
2026-05-17 13:02:53 -0300

    construtor feito

--- Grafo.cpp
+++ Grafo.cpp
@@ -1,2 +1,10 @@
 #include "Grafo.h"
 
+//construtor
+Grafo::Grafo(int n, bool orientado_grafo, bool ponderado_grafo){
+    numVertices = n;
+    orientado = orientado_grafo;
+    ponderado = ponderado_grafo;
+
+    matriz.resize(n, vector<int>(n, 0));
+}
